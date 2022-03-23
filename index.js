1dda9ee7b80853d96304b3b681cafa5f0e74682b
Jacky Liang <asheriffy@gmail.com>
2022-03-23 09:59:21 -0400

    commit tree 39c3440833c8882b09ceab50b7c916b8e8bc2c70

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "blue";
+
+// document.addEventListener("keydown", function (event) {
+//     console.log(event);
+//   });
+
+
+// document.addEventListener("keydown", function (event) {
+//     if (event.key === "ArrowLeft") {
+//       const leftNumbers = dodger.style.left.replace("px", "");
+//       const left = parseInt(leftNumbers, 10);
+  
+//       dodger.style.left = `${left - 3}px`;
+//     }
+//   });
+
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 3}px`;
+  }
+
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 400 ) {
+      dodger.style.left = `${left + 3}px`;
+    }
+  
+  }
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  });
